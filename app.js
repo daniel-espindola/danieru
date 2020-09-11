81fd957edbf16980e3863ce5f18025f75962e688
Daniel Espindola da Silva <espindola.daniel@outlook.com.br>
2020-09-10 22:39:56 -0300

    sketch finalizado

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 let allFlowers = [];
 
 const rngPos = (w, h, fw, fh) => {
+  //Retorna uma posição possível para uma nova flor, checa se há colisões.
   let safe = false;
   let x = 0;
   let y = 0;
@@ -35,6 +36,7 @@ const randSign = () => {
   return Math.random() > 0.25 ? 1 : -1;
 };
 
+//Pega o tamanho da viewport
 const vw = Math.max(
   document.documentElement.clientWidth || 0,
   window.innerWidth || 0
@@ -44,30 +46,30 @@ const vh = Math.max(
   window.innerHeight || 0
 );
 
+// Cálcula um número máximo de flores que seja garantido de caber na viewport sem colisões
 let fheight = 48;
 let maxFlowers = ((vw / fheight) * (vh / fheight)) / 4;
 
-for (let index = 0; index < Math.trunc(maxFlowers / 2); index++) {
-  console.log(maxFlowers);
+// Popula a viewport com flores
+for (let index = 0; index < Math.trunc(maxFlowers); index++) {
   let flower = document.createElement("img");
-  flower.id = 'flower'
+  flower.id = "flower";
   flower.src = "lunar-tear.png";
   flower.alt = "lunar tear from the nier game series";
-  flower.classList.add('lunar')
+  flower.classList.add('lunar');
   flower.height = fheight;
   flower.style.transform = `scaleX(${randSign()}) rotate(${
     Math.random() * 20
   }deg)`;
-  flower.style.animation = `6s ease glow infinite`;
+  flower.style.animation = `4s ease ${-Math.random() *.5}s glow infinite`;
   let pos = rngPos(vw, vh, flower.height, flower.height);
   allFlowers.push(pos);
   flower.style.top = pos.y + "px";
   flower.style.left = pos.x + "px";
-
-
   document.body.appendChild(flower);
 }
 
+//Coloca kaine salvation como evento do click de alguma flor
 let flower = document.getElementById('flower')
 flower.addEventListener("click", () => {
   let audio = document.getElementById('music');
